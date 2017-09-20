14a57742c3239566e188248b62c59f169ee0556b
Nate <tel.akos.2@gmail.com>
2017-09-20 11:03:43 +0200

    Done with two exercises

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+import java.lang.Math;
+
+public class Main
+{
+  
+  public static void main(String[] args)
+  {
+    Factorializator fac = new Factorializator();
+    System.out.println(fac.factorialize(5));
+    
+    StringReverser rev = new StringReverser();
+    System.out.println(rev.reverse("Hey, Vsauce!"));
+  }
+}
+
+
